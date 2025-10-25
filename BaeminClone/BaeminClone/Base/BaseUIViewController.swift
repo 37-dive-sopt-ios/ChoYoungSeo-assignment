367509e0682ee8d96a205cf46b144a3541487948
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-10-25 21:20:33 +0900

    [chore] 기본 네비게이션바 제거 및 배경색 지정

--- BaeminClone/BaeminClone/Base/BaseUIViewController.swift
+++ BaeminClone/BaeminClone/Base/BaseUIViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 class BaseViewController: UIViewController {
     
     // MARK: - Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         setUI()
         setLayout()
     }
     
     // MARK: - Setup Methods
+    
     func setUI() {}
     func setLayout() {}
 }
