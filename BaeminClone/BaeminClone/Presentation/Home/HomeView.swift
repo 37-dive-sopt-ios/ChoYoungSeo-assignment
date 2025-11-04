04eb44b8eb7b3ea37b6a374d40717270c1547d36
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2025-11-04 13:27:44 +0900

    [chore] 홈뷰 기초세팅

---
+++ BaeminClone/BaeminClone/Presentation/Home/HomeView.swift
@@ -0,0 +1,14 @@
+//
+//  HomeView.swift
+//  BaeminClone
+//
+//  Created by 조영서 on 11/1/25.
+//
+
+
+import UIKit
+import SnapKit
+import Then
+
+final class HomeView: BaseUIView {
+}
---
+++ BaeminClone/BaeminClone/Presentation/Home/HomeViewController.swift
@@ -0,0 +1,11 @@
+//
+//  HomeViewController.swift
+//  BaeminClone
+//
+//  Created by 조영서 on 11/1/25.
+//
+
+import UIKit
+
+final class HomeViewController: BaseViewController {
+}
